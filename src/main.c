e059c9758ea8150141282e902b3c11017c81213c
YoMin Su <m11317004@yuntech.edu.tw>
2024-11-06 12:49:05 +0800

    2024/11/06
    使用Zephyr官方formatting整理程式碼

--- src/main.c
+++ src/main.c
@@ -1,46 +1,41 @@
-#include <zephyr/kernel.h>
+#include <stdio.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
-#include <stdio.h>
+#include <zephyr/kernel.h>
 
 const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(shtc3));
 
-int main(void) {
-    int rc;
+int main(void)
+{
+	int rc;
 
-    if (!device_is_ready(dev)) {
-        printf("Device %s is not ready\n", dev->name);
-        return 0;
-    }
+	if (!device_is_ready(dev)) {
+		printf("Device %s is not ready\n", dev->name);
+		return 0;
+	}
 
-    while (true) {
-        struct sensor_value temp, hum;
-        rc = sensor_sample_fetch(dev);
-        
-        /*get temp & hum*/
-        if(rc == 0) {
-            rc = sensor_channel_get(
-                dev, 
-                SENSOR_CHAN_AMBIENT_TEMP,
-                &temp);
-        }
-        if (rc == 0) {
-            rc = sensor_channel_get(
-                dev, 
-                SENSOR_CHAN_HUMIDITY,
-                &hum);
-        }
-        /*get temp & hum*/
+	while (true) {
+		struct sensor_value temp, hum;
+		rc = sensor_sample_fetch(dev);
 
-        /*error handle*/
-        if (rc!= 0) {
-            printf("shtc3 error, code: %d", rc);
-            break;
-        }
-        
-        printf("Temperature: %.2f °C\n", sensor_value_to_double(&temp));
-        printf("Humidity: %.2f %%\n", sensor_value_to_double(&hum));
+		/*get temp & hum*/
+		if (rc == 0) {
+			rc = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		}
+		if (rc == 0) {
+			rc = sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum);
+		}
+		/*get temp & hum*/
 
-        k_sleep(K_SECONDS(2));
-    } 
+		/*error handle*/
+		if (rc != 0) {
+			printf("shtc3 error, code: %d", rc);
+			break;
+		}
+
+		printf("Temperature: %.2f °C\n", sensor_value_to_double(&temp));
+		printf("Humidity: %.2f %%\n", sensor_value_to_double(&hum));
+
+		k_sleep(K_SECONDS(2));
+	}
 }
